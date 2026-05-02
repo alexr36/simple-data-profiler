088e718f69fd92d97405fd85417a357ec76be761
Alex <rogozinskialex@gmail.com>
2026-05-02 12:41:10 +0200

    Added possibility to speficy output path for the generated HTML report.

--- data_profiler.py
+++ data_profiler.py
@@ -6,23 +6,24 @@ from pandas import read_csv
 from data_profiling import ProfileReport
 
 
-def generate_report(filename: str = 'data/dane.csv') -> None:
+def generate_report(filename: str, output: str) -> None:
     '''
     Generate a profiling report for the given .csv file.
 
     Params:
         filename (str): Path to the .csv file to profile.
-    
+        output (str): Path to the output HTML report.   
     Returns:
         None
     '''
     df = read_csv(filename)
     profile = ProfileReport(df, title=f'{filename} - Profiling Report', explorative=True)
-    profile.to_file('report.html')
+    profile.to_file(output)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Simple data profiler for .csv files.')
     parser.add_argument('-f', '--file', help='Path to .csv file', default='data/dane.csv')
+    parser.add_argument('-o', '--output', help='Path to output HTML report', default='report.html')
     args = parser.parse_args()
-    generate_report(args.file)
+    generate_report(args.file, args.output)
